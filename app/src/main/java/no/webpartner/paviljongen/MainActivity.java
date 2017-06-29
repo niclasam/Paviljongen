cdb57efc8b9059e88431a30b94771fe1793565b9
niclasam <niclas@hotmail.no>
2017-06-29 12:47:45 +0200

    Initial commit

---
+++ app/src/main/java/no/webpartner/paviljongen/MainActivity.java
@@ -0,0 +1,13 @@
+package no.webpartner.paviljongen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
